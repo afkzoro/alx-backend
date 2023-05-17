8f837cdc4213f3a6381b33dff60f5cca93e6f807
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-17 13:32:02 +0100

    implement get_page method

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """a method named get_page that takes two integer arguments page
+        with default value 1 and page_size with default value 10
+
+        Args:
+            page (int, optional): _description_. Defaults to 1.
+            page_size (int, optional): _description_. Defaults to 10.
+
+        Returns:
+            List[List]: appropriate subset of the dataset using
+            the calculated indices.
+        """
+        assert isinstance(page, int) and page > 0, "Integer is greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "Integer is > 0"
+
+        dataset = self.dataset()
+        total_pages = math.ceil(len(dataset) / page_size)
+
+        if page > total_pages:
+            return []
+
+        start_index, end_index = index_range(page, page_size)
+        return dataset[start_index:end_index]
