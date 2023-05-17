30ff4401007a9e10dde427543b0ac11f678cf563
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-17 13:11:27 +0100

    commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 """ Function `index_range`
 Takes  two integer arguments page and page_size
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ a function named index_range that takes
     two integer arguments page and page_size
 
@@ -17,4 +18,4 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return start_index, end_index
+    return (start_index, end_index)
