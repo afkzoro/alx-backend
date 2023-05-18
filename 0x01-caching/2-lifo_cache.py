9de586cb4d59dbc0df454d57b0636e1abddd02da
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-18 12:25:43 +0100

    commit

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,21 +10,24 @@ class LIFOCache(BaseCaching):
         """Init method
         """
         super().__init__()
-        self.keys_order = []
 
     def put(self, key, item):
-        """ Put method """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
-            self.keys_order.append(key)
+        """ put method
+        """
+        if key is None or item is None:
+            return
 
-            if len(self.keys_order) > BaseCaching.MAX_ITEMS:
-                last_key = self.keys_order.pop()
-                del self.cache_data[last_key]
-                print(f"DISCARD: {last_key}")
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            discarded_key = list(self.cache_data.keys())[-2]
+            print(f"DISCARD: {discarded_key}")
+            del self.cache_data[discarded_key]
 
     def get(self, key):
-        """ get method """
-        if key is not None:
-            return self.cache_data.get(key)
-        return None
+        """ get method
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
