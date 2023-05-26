e491c56cc78871a3c93116be70f8e60a8ec9605f
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-26 09:40:19 +0100

    Fix rendered template

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def index() -> str:
     """ index function
     """
-    return render_template('1-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
