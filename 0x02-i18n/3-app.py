89e81cd1ee507fe9044a9a70387b3f434a4b5ecf
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-26 13:45:00 +0100

    update files

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Task-1 Flask-Babel """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Babel config
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ index function
+    """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    This function determines the best-matching language based
+    on the Accept-Language header of the request.
+
+    It selects the language that matches the supported languages in the order
+    of preference.
+
+    Returns:
+        str: The selected language code.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
