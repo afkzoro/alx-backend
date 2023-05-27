4577e48c4994fee9c5f2b919ea0047d401f6f161
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-27 11:24:19 +0100

    update app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -13,6 +13,7 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_TRANSLATION_DIRECTORIES = 'translations'
 
 
 app.config.from_object(Config)
